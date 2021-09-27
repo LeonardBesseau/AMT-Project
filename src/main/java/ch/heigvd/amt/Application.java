1761b1d2ff5da7636735d7e8f9df3c6ec556cb94
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-09-27 17:58:18 +0200

    Added jdbi interface and mapper for product

--- src/main/java/ch/heigvd/amt/Application.java
+++ src/main/java/ch/heigvd/amt/Application.java
@@ -1,7 +1,18 @@
 package ch.heigvd.amt;
-
+import ch.heigvd.amt.models.Product;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.reflect.ConstructorMapper;
+import org.jdbi.v3.postgres.PostgresPlugin;
 
 public class Application {
 
+  public static void main(String[] args) {
+    Jdbi jdbi = Jdbi.create("jdbc:postgresql://localhost:5432/amt?user=amt&password=amt");
+    jdbi.installPlugin(new PostgresPlugin());
+    jdbi.registerRowMapper(ConstructorMapper.factory(Product.class));
+    jdbi.useHandle(handle -> {
+      handle.createQuery("SELECT * FROM products").mapTo(Product.class).forEach(System.out::println);
+    });
+  }
 
 }
--- src/main/java/ch/heigvd/amt/models/Product.java
+++ src/main/java/ch/heigvd/amt/models/Product.java
@@ -1,21 +1,21 @@
 package ch.heigvd.amt.models;
 
+import java.beans.ConstructorProperties;
+
 public class Product {
 
-    private String name;
-    private double price;
-    private String description;
-    private long id;
-    private String[] tags;
+    private final String name;
+    private final double price;
+    private final String description;
+    private final String refCode;
+    private final String tags;
 
-    public Product() {
-    }
-
-    public Product(String name, double price, String description, long id, String[] tags) {
+    @ConstructorProperties({"name", "price", "description", "refCode", "tags"})
+    public Product(String name, double price, String description, String refCode, String tags) {
         this.name = name;
         this.price = price;
         this.description = description;
-        this.id = id;
+        this.refCode = refCode;
         this.tags = tags;
     }
 
@@ -23,39 +23,30 @@ public class Product {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getPrice() {
         return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public String getRefCode() {
+        return refCode;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String[] getTags() {
+    public String getTags() {
         return tags;
     }
 
-    public void setTags(String[] tags) {
-        this.tags = tags;
+    @Override
+    public String toString() {
+        return "Product{" +
+            "name='" + name + '\'' +
+            ", price=" + price +
+            ", description='" + description + '\'' +
+            ", refCode='" + refCode + '\'' +
+            ", tags='" + tags + '\'' +
+            '}';
     }
 }
