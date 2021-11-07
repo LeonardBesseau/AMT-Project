d8bafeb649e97b83ab784c377287933383fb8c65
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-11-07 12:12:10 +0100

    Applied spotless

--- src/main/java/ch/heigvd/amt/models/Category.java
+++ src/main/java/ch/heigvd/amt/models/Category.java
@@ -16,8 +16,6 @@ public class Category {
 
   @Override
   public String toString() {
-    return "Category{" +
-        "name='" + name + '\'' +
-        '}';
+    return "Category{" + "name='" + name + '\'' + '}';
   }
 }
