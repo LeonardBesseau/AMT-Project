36a1a0154e9e011887b9282e68471a83fed8b62c
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-11-15 16:28:18 +0100

    creating cookies

--- src/main/java/ch/heigvd/amt/view/LoginView.java
+++ src/main/java/ch/heigvd/amt/view/LoginView.java
@@ -7,27 +7,13 @@ import io.quarkus.qute.TemplateInstance;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/view/login")
+// A EVENTUELLEMENT DEPLACER DANS LA RESSOURCE
+
 @ApplicationScoped
 public class LoginView {
 
-    @Inject
-    @Location("LoginView/login.html")
-    Template login;
-
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance getLoginPage(@HeaderParam("PasswordsNotMatching") boolean arePasswordsNotMatching) {
-
-        if (arePasswordsNotMatching) {
-            return login.data("errorPassword", "Passwords do not match");
-        }
-
-        return login.data("errorPassword", "");
-    }
 }
