7069119112b85ee16fc2aa9cd23677a377530de3
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-11-19 23:09:29 +0100

    Added filter for product by category in view

--- src/main/resources/META-INF/resources/js/utils.js
+++ src/main/resources/META-INF/resources/js/utils.js
@@ -1,13 +1,15 @@
 function sort(newFilter){
   if( typeof this.filters === 'undefined'){
-    this.filters = [];
+    this.filters = []; // remember activated filters
   }
   if(this.filters.includes(newFilter)){
+    // remove filter
     this.filters = this.filters.filter((item) => {return item !== newFilter});
     [...document.getElementsByClassName("category-selector-"+newFilter)].forEach((item) =>{
       item.style.background = "transparent"
     });
   }else{
+    // add filter
     this.filters.push(newFilter);
     [...document.getElementsByClassName("category-selector-"+newFilter)].forEach((item) =>{
       console.log(item.style.background)
@@ -15,10 +17,12 @@ function sort(newFilter){
     });
   }
 
+  // hide all
   [...document.getElementsByClassName("toFilter")].forEach((item) =>{
     item.style.display = "none"
   });
 
+  // display selected
   [...document.getElementsByClassName("toFilter")].filter((item) => {
     for (let filter of this.filters){
       if (item.className.indexOf(filter) === -1){
