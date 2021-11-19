871098aeea114d9d5ff2de551e3aa65a58691e4b
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-11-19 18:06:56 +0100

    Cookies working

--- src/main/java/ch/heigvd/amt/resources/LoginResource.java
+++ src/main/java/ch/heigvd/amt/resources/LoginResource.java
@@ -5,70 +5,56 @@ import io.quarkus.logging.Log;
 import io.quarkus.qute.Location;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.cookie.BasicClientCookie;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Cookie;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Date;
 import java.util.Objects;
 
+@Path("/login")
 public class LoginResource {
 
     private static final String REGISTER_ERROR = "registerError";
     private static final String LOGIN_ERROR    = "loginError";
-    private static final String AUTHSERV_ADDR  = "http://10.0.1.192:8080";
+    private static final String AUTHSERV_ADDR  = "http://localhost:8082";
 
     @Inject
     @Location("LoginView/login.html")
     Template login;
 
     @GET
-    @Path("/view/login")
+    @Path("/view")
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance getLoginPage() {
         return login.data(REGISTER_ERROR, null).data(LOGIN_ERROR, null);
     }
 
     @POST
-    @Path("/login/signin")
+    @Path("/signin")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_HTML)
     public Object connectToExistingAccount(@FormParam("username") String username, @FormParam("password") String password) {
 
         try {
-            CloseableHttpResponse response = sendToAuthServ("/auth/login", username, password);
-            String body = EntityUtils.toString(response.getEntity());
+            Response response = sendToAuthServ("/auth/login", username, password);
+            String body = response.readEntity(String.class);
 
-            switch(response.getStatusLine().getStatusCode()) {
-                case HttpStatus.SC_OK:
-
-                    JSONObject jsonBody = new JSONObject(body);
-                    String token = jsonBody.getString("token");
-                    String role = jsonBody.getJSONObject("account").getString("role");
-
-                    // Cookie duration : 1 day in seconds
-                    int cookieDuration = 3600 * 24;
-                    NewCookie cookieJWT = new NewCookie("jwt_token", token, "", null, null, cookieDuration, false, true);
-                    NewCookie cookieRole = new NewCookie("user_role", role, "", null, null, cookieDuration, false, false);
-
-                    return Response.status(Response.Status.MOVED_PERMANENTLY).cookie(cookieJWT, cookieRole).location(URI.create("/view/product")).build();
-                case HttpStatus.SC_FORBIDDEN:
-                    return login.data(REGISTER_ERROR, null).data(LOGIN_ERROR, "Username or password incorrect.");
+            switch(response.getStatusInfo().getStatusCode()){
+                case 200:
+                    NewCookie[] cookies = createCookies(body);
+                    return Response.status(Response.Status.MOVED_PERMANENTLY).cookie(cookies[0], cookies[1]).location(URI.create("/view/product")).build();
+                case 403:
+                    return login.data(REGISTER_ERROR, null, LOGIN_ERROR, "Username or password incorrect.");
                 default:
-                    return login.data(REGISTER_ERROR, null).data(LOGIN_ERROR, "Unknown error.");
+                    return login.data(REGISTER_ERROR, null, LOGIN_ERROR, "Unknown error.");
             }
         }
         catch (IOException e) {
@@ -78,29 +64,32 @@ public class LoginResource {
     }
 
     @POST
-    @Path("/login/signup")
+    @Path("/signup")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_HTML)
     public Object createNewAccount(@FormParam("username") String username, @FormParam("password") String password,
                                      @FormParam("confirmPassword") String confirmPassword) {
 
         if (!password.equals(confirmPassword)) {
-            return login.data(REGISTER_ERROR, "Passwords do not match.");
+            return login.data(REGISTER_ERROR, "Passwords do not match.", LOGIN_ERROR, null);
         }
 
         try {
-            CloseableHttpResponse response = sendToAuthServ("/accounts/register", username, password);
+            Response response = sendToAuthServ("/accounts/register", username, password);
+            String body = response.readEntity(String.class);
 
-            switch(response.getStatusLine().getStatusCode()) {
-                case HttpStatus.SC_CREATED:
-                    return Response.status(Response.Status.CREATED).entity("Account created").build();
-                case HttpStatus.SC_CONFLICT:
-                    break;
-                case HttpStatus.SC_UNPROCESSABLE_ENTITY:
+            switch(response.getStatusInfo().getStatusCode()) {
+                case 201:
+                    NewCookie[] cookies = createCookies(body);
+                    return Response.status(Response.Status.CREATED).cookie(cookies[0], cookies[1]).entity("Account created").build();
+                case 409:
+                    return login.data(REGISTER_ERROR, "Passwords do not match.", LOGIN_ERROR, null);
+                case 422:
                     break;
                 default:
             }
 
-            String result = EntityUtils.toString(response.getEntity());
+            String result = response.readEntity(String.class);
         }
         catch (IOException e) {
 
@@ -108,26 +97,31 @@ public class LoginResource {
         return null;
     }
 
-    private CloseableHttpResponse sendToAuthServ(String resource, String username, String password) throws IOException {
+    private Response sendToAuthServ(String resource, String username, String password) throws IOException {
 
         Objects.requireNonNull(resource);
         Objects.requireNonNull(username);
         Objects.requireNonNull(password);
 
-        HttpPost post = new HttpPost(AUTHSERV_ADDR + resource);
-        post.addHeader("Content-Type", "application/json");
-        post.addHeader("Accept", "application/json");
-
         JSONObject jsonInput = new JSONObject();
         jsonInput.put("username", username);
         jsonInput.put("password", password);
 
-        // send a JSON data
-        post.setEntity(new StringEntity(jsonInput.toString()));
+        Client client = ClientBuilder.newClient();
+        return client.target(AUTHSERV_ADDR).path(resource).request(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).post(Entity.json(jsonInput));
+    }
 
+    private NewCookie[] createCookies(String ResponseBody) {
 
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            return httpClient.execute(post);
-        }
+        JSONObject jsonBody = new JSONObject(ResponseBody);
+        String token = jsonBody.getString("token");
+        String role = jsonBody.getJSONObject("account").getString("role");
+
+        NewCookie cookieJWT = new NewCookie("jwt_token", token, "/", "localhost",
+                "", -1, false, true);
+        NewCookie cookieRole = new NewCookie("user_role", role, "/", "localhost",
+                "", -1, false, false);
+
+        return new NewCookie[]{cookieJWT, cookieRole};
     }
 }
