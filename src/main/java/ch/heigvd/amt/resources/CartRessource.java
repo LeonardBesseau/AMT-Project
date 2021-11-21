4d57bfeeb1bd5aa990839b3b3b1ef3bc2c37aa6a
FionaGbn <fiona.gamboni@heig-vd.ch>
2021-11-21 17:15:48 +0100

    added cart management

---
+++ src/main/java/ch/heigvd/amt/resources/CartRessource.java
@@ -0,0 +1,35 @@
+package ch.heigvd.amt.resources;
+
+import ch.heigvd.amt.models.Product;
+import io.quarkus.qute.Location;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/cart")
+public class CartRessource {
+
+    // Inject the template html.
+    @Inject
+    @Location("CartView/cart.html")
+    Template cart;
+
+    /**
+     * Get the cart page corresponding to the user session or simple visitor
+     *
+     * @return a html page of the cart
+     */
+    @GET
+    @Path("/view")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance getCart() {
+        // #TODO(check for session and get specific member cart)
+        return cart.data("isMember", false);
+    }
+
+}
