907fd08a99ce9d7caa69367ff074f94faa4549b7
FionaGbn <fiona.gamboni@heig-vd.ch>
2021-11-21 18:16:07 +0100

    spotless you know

--- src/main/java/ch/heigvd/amt/resources/CartRessource.java
+++ src/main/java/ch/heigvd/amt/resources/CartRessource.java
@@ -1,10 +1,8 @@
 package ch.heigvd.amt.resources;
 
-import ch.heigvd.amt.models.Product;
 import io.quarkus.qute.Location;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -14,22 +12,21 @@ import javax.ws.rs.core.MediaType;
 @Path("/cart")
 public class CartRessource {
 
-    // Inject the template html.
-    @Inject
-    @Location("CartView/cart.html")
-    Template cart;
+  // Inject the template html.
+  @Inject
+  @Location("CartView/cart.html")
+  Template cart;
 
-    /**
-     * Get the cart page corresponding to the user session or simple visitor
-     *
-     * @return a html page of the cart
-     */
-    @GET
-    @Path("/view")
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance getCart() {
-        // #TODO(check for session and get specific member cart)
-        return cart.data("isMember", false);
-    }
-
+  /**
+   * Get the cart page corresponding to the user session or simple visitor
+   *
+   * @return a html page of the cart
+   */
+  @GET
+  @Path("/view")
+  @Produces(MediaType.TEXT_HTML)
+  public TemplateInstance getCart() {
+    // #TODO(check for session and get specific member cart)
+    return cart.data("isMember", false);
+  }
 }
