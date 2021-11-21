6c88429823d21b6bd066f80e3d18afc113659158
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-11-21 16:57:49 +0100

    comments considered

--- src/main/java/ch/heigvd/amt/resources/LoginResource.java
+++ src/main/java/ch/heigvd/amt/resources/LoginResource.java
@@ -1,12 +1,18 @@
 package ch.heigvd.amt.resources;
 
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.quarkus.logging.Log;
 import io.quarkus.qute.Location;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import org.json.JSONObject;
-
+import java.io.IOException;
+import java.net.URI;
+import java.util.Base64;
+import java.util.Objects;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.client.Client;
@@ -15,123 +21,198 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
-import java.net.URI;
-import java.util.Base64;
-import java.util.Objects;
 
 @Path("/login")
 public class LoginResource {
 
-    private static final String REGISTER_ERROR   = "registerError";
-    private static final String REGISTER_SUCCESS = "registerSuccess";
-    private static final String LOGIN_ERROR      = "loginError";
-    private static final String LOGGED           = "logged";
-    private static final String AUTHSERV_ADDR    = "http://localhost:8082";
+  private static final String REGISTER_ERROR = "registerError";
+  private static final String REGISTER_SUCCESS = "registerSuccess";
+  private static final String LOGIN_ERROR = "loginError";
+  private static final String LOGGED = "logged";
+  private static final String AUTHSERV_ADDR = "http://localhost:8082";
 
-    @Inject
-    @Location("LoginView/login.html")
-    Template login;
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    @GET
-    @Path("/view")
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance getLoginPage(@CookieParam("jwt_token") NewCookie jwtToken, @CookieParam("user_role") NewCookie userRole) {
+  @Inject
+  @Location("LoginView/login.html")
+  Template login;
 
-        if (jwtToken != null && userRole != null) {
+  @GET
+  @Path("/view")
+  @Produces(MediaType.TEXT_HTML)
+  public TemplateInstance getLoginPage(
+      @CookieParam("jwt_token") NewCookie jwtToken, @CookieParam("user_role") NewCookie userRole) {
 
-            String[] chunks = jwtToken.toString().split("\\.");
-            JSONObject payload = new JSONObject(new String(Base64.getDecoder().decode(chunks[1])));
-            return login.data(REGISTER_SUCCESS, null, REGISTER_ERROR, null, LOGIN_ERROR, null,
-                    LOGGED, payload.getString("role") + " : " + payload.getString("sub"));
-        }
-        return login.data(REGISTER_SUCCESS, null, REGISTER_ERROR, null, LOGIN_ERROR, null, LOGGED, null);
+    try {
+      if (jwtToken != null && userRole != null) {
+
+        String[] userInfo = getUserInfo(jwtToken);
+
+        // Only for demonstration purpose, when merged in dev, need to redirect to the main page if
+        // already connected
+        // /product/admin/view or /product/view
+        return login.data(
+            REGISTER_SUCCESS,
+            null,
+            REGISTER_ERROR,
+            null,
+            LOGIN_ERROR,
+            null,
+            LOGGED,
+            userInfo[1] + " : " + userInfo[0]);
+      }
+      return login.data(
+          REGISTER_SUCCESS, null, REGISTER_ERROR, null, LOGIN_ERROR, null, LOGGED, null);
+    } catch (JsonProcessingException e) {
+      Log.error("JsonProcessingException occured");
+    }
+    return null;
+  }
+
+  @POST
+  @Path("/signin")
+  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+  @Produces(MediaType.TEXT_HTML)
+  public Object connectToExistingAccount(
+      @FormParam("username") String username, @FormParam("password") String password) {
+
+    try {
+      Response response = sendToAuthServ("/auth/login", username, password);
+      String body = response.readEntity(String.class);
+
+      // we keep the switch structure if we have to add codes that produce different behaviours
+      switch (response.getStatusInfo().getStatusCode()) {
+        case 200:
+          NewCookie[] cookies = createCookies(body);
+          String resource = "/product/view";
+          if (getUserInfo(cookies[0])[1].equals("admin")) {
+            resource = "/product/admin/view";
+          }
+          return Response.status(Response.Status.MOVED_PERMANENTLY)
+              .cookie(cookies[0], cookies[1])
+              .location(URI.create(resource))
+              .build();
+        default: // currently 403
+          return login.data(
+              REGISTER_SUCCESS,
+              null,
+              REGISTER_ERROR,
+              null,
+              LOGIN_ERROR,
+              OBJECT_MAPPER.readTree(body).get("error").asText(),
+              LOGGED,
+              null);
+      }
+    } catch (IOException e) {
+      Log.error("IOException occured");
+    }
+    return null;
+  }
+
+  @POST
+  @Path("/signup")
+  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+  @Produces(MediaType.TEXT_HTML)
+  public Object createNewAccount(
+      @FormParam("username") String username,
+      @FormParam("password") String password,
+      @FormParam("confirmPassword") String confirmPassword) {
+
+    if (!password.equals(confirmPassword)) {
+      return login.data(
+          REGISTER_SUCCESS,
+          null,
+          REGISTER_ERROR,
+          "Passwords do not match",
+          LOGIN_ERROR,
+          null,
+          LOGGED,
+          null);
     }
 
-    @POST
-    @Path("/signin")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.TEXT_HTML)
-    public Object connectToExistingAccount(@FormParam("username") String username, @FormParam("password") String password) {
+    try {
+      Response response = sendToAuthServ("/accounts/register", username, password);
+      String body = response.readEntity(String.class);
 
-        try {
-            Response response = sendToAuthServ("/auth/login", username, password);
-            String body = response.readEntity(String.class);
-
-            // we keep the switch structure if we have to add codes that produce different behaviours
-            switch(response.getStatusInfo().getStatusCode()){
-                case 200:
-                    NewCookie[] cookies = createCookies(body);
-                    return Response.status(Response.Status.MOVED_PERMANENTLY).cookie(cookies[0], cookies[1]).location(URI.create("/view/product")).build();
-                default: // currently 403
-                    return login.data(REGISTER_SUCCESS, null, REGISTER_ERROR, null, LOGIN_ERROR, new JSONObject(body).getString("error"), LOGGED, null);
-            }
-        }
-        catch (IOException e) {
-            Log.error("IOException occured");
-        }
-        return null;
+      switch (response.getStatusInfo().getStatusCode()) {
+        case 201:
+          return login.data(
+              REGISTER_SUCCESS,
+              "Account created",
+              REGISTER_ERROR,
+              null,
+              LOGIN_ERROR,
+              null,
+              LOGGED,
+              null);
+        case 409:
+          return login.data(
+              REGISTER_SUCCESS,
+              null,
+              REGISTER_ERROR,
+              OBJECT_MAPPER.readTree(body).get("error").asText(),
+              LOGIN_ERROR,
+              null,
+              LOGGED,
+              null);
+        case 422:
+          return login.data(
+              REGISTER_SUCCESS,
+              null,
+              REGISTER_ERROR,
+              OBJECT_MAPPER.readTree(body).get("errors").get(0).get("message").asText(),
+              LOGIN_ERROR,
+              null,
+              LOGGED,
+              null);
+      }
+    } catch (IOException e) {
+      Log.error("IOException occured");
     }
+    return null;
+  }
 
-    @POST
-    @Path("/signup")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.TEXT_HTML)
-    public Object createNewAccount(@FormParam("username") String username, @FormParam("password") String password,
-                                     @FormParam("confirmPassword") String confirmPassword) {
+  private Response sendToAuthServ(String resource, String username, String password)
+      throws IOException {
 
-        if (!password.equals(confirmPassword)) {
-            return login.data(REGISTER_SUCCESS, null, REGISTER_ERROR, "Passwords do not match", LOGIN_ERROR, null, LOGGED, null);
-        }
+    Objects.requireNonNull(resource);
+    Objects.requireNonNull(username);
+    Objects.requireNonNull(password);
 
-        try {
-            Response response = sendToAuthServ("/accounts/register", username, password);
-            String body = response.readEntity(String.class);
+    ObjectNode jsonInput = JsonNodeFactory.instance.objectNode();
+    jsonInput.put("username", username);
+    jsonInput.put("password", password);
 
-            switch(response.getStatusInfo().getStatusCode()) {
-                case 201:
-                    return login.data(REGISTER_SUCCESS, "Account created", REGISTER_ERROR, null, LOGIN_ERROR, null, LOGGED, null);
-                case 409:
-                    return login.data(REGISTER_SUCCESS, null, REGISTER_ERROR, new JSONObject(body).getString("error"), LOGIN_ERROR, null, LOGGED, null);
-                case 422:
-                    return login.data(REGISTER_SUCCESS, null,
-                            REGISTER_ERROR, new JSONObject(body).getJSONArray("errors").getJSONObject(0).getString("message"),
-                            LOGIN_ERROR, null, LOGGED, null);
-            }
-        }
-        catch (IOException e) {
-            Log.error("IOException occured");
-        }
-        return null;
-    }
+    Client client = ClientBuilder.newClient();
+    return client
+        .target(AUTHSERV_ADDR)
+        .path(resource)
+        .request(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .post(Entity.json(jsonInput.toString()));
+  }
 
-    private Response sendToAuthServ(String resource, String username, String password) throws IOException {
+  private NewCookie[] createCookies(String ResponseBody) throws JsonProcessingException {
 
-        Objects.requireNonNull(resource);
-        Objects.requireNonNull(username);
-        Objects.requireNonNull(password);
+    Objects.requireNonNull(ResponseBody);
 
-        JSONObject jsonInput = new JSONObject();
-        jsonInput.put("username", username);
-        jsonInput.put("password", password);
+    JsonNode jsonBody = OBJECT_MAPPER.readTree(ResponseBody);
+    String token = jsonBody.get("token").asText();
+    String role = jsonBody.get("account").get("role").asText();
 
-        Client client = ClientBuilder.newClient();
-        return client.target(AUTHSERV_ADDR).path(resource).request(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).post(Entity.json(jsonInput.toString()));
-    }
+    NewCookie cookieJWT = new NewCookie("jwt_token", token, "/", "localhost", "", -1, false, true);
+    NewCookie cookieRole = new NewCookie("user_role", role, "/", "localhost", "", -1, false, false);
 
-    private NewCookie[] createCookies(String ResponseBody) {
+    return new NewCookie[] {cookieJWT, cookieRole};
+  }
 
-        Objects.requireNonNull(ResponseBody);
+  private String[] getUserInfo(NewCookie jwtToken) throws JsonProcessingException {
 
-        JSONObject jsonBody = new JSONObject(ResponseBody);
-        String token = jsonBody.getString("token");
-        String role = jsonBody.getJSONObject("account").getString("role");
+    Objects.requireNonNull(jwtToken);
 
-        NewCookie cookieJWT = new NewCookie("jwt_token", token, "/", "localhost",
-                "", -1, false, true);
-        NewCookie cookieRole = new NewCookie("user_role", role, "/", "localhost",
-                "", -1, false, false);
+    String[] chunks = jwtToken.toString().split("\\.");
+    JsonNode payload = OBJECT_MAPPER.readTree(new String(Base64.getDecoder().decode(chunks[1])));
 
-        return new NewCookie[]{cookieJWT, cookieRole};
-    }
+    return new String[] {payload.get("sub").asText(), payload.get("role").asText()};
+  }
 }
