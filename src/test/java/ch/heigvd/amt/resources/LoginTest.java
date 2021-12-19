a679a7806ffd16a238ca22cfaca5119ec2e10417
K-do <miguel.dovalelopes@heig-vd.ch>
2021-12-19 20:25:37 +0100

    lint the way

--- src/test/java/ch/heigvd/amt/resources/LoginTest.java
+++ src/test/java/ch/heigvd/amt/resources/LoginTest.java
@@ -1,11 +1,8 @@
 package ch.heigvd.amt.resources;
 
-import static io.restassured.RestAssured.given;
-
 import ch.heigvd.amt.mock.MockServerExtension;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 @QuarkusTestResource(MockServerExtension.class)
